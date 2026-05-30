f6e7817b5e8e8d5786b6838af2012cfc876b6a3f
Larry <larry.anderson.jr95@gmail.com>
2026-05-30 18:47:37 -0500

    initial commit

---
+++ src/test/java/com/wexinc/purchaseconverter/WexTransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wexinc.purchaseconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WexTransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
